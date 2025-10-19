3e05826c7957be3d79d3e9ab4a75c2bda88d8a19
shindelokesh <you@example.com>
2025-10-19 09:59:34 +0530

    tt

--- ctk_login_forum.py
+++ ctk_login_forum.py
@@ -6,12 +6,20 @@ app.title("Login Form")
 app.geometry("400x300")
 
 
-label_4 = ctk.CTkLabel(app, text="Login Form", text_color="white", font=("ranan", 30))
-label_4.pack(pady=40)
+
+
+
+title_label = ctk.CTkLabel(
+    app,
+    text="Login Form",
+    font=("Times New Roman", 40, "bold"),
+    text_color="white"
+)
+title_label.pack(pady=20)
 
 #frame settings
 frame = ctk.CTkFrame(app)
-frame.pack(pady=50)
+frame.pack(pady=20)
 
 
 # Labels and entries
@@ -21,7 +29,7 @@ label_1.grid(row=0, column=0, padx=30, pady=10)
 entry_1 = ctk.CTkEntry(frame, width=200)
 entry_1.grid(row=0, column=1, padx=30, pady=10)
 
-label_2 = ctk.CTkLabel(frame, text="Enter URL:", text_color="white", font=("ranan", 25))
+label_2 = ctk.CTkLabel(frame, text="Enter URL:", text_color="white", font=("ranan", 25,))
 label_2.grid(row=1, column=0, padx=30, pady=10)
 entry_2 = ctk.CTkEntry(frame, width=200)
 entry_2.grid(row=1, column=1, padx=30, pady=10)
@@ -43,17 +51,17 @@ def submit_action():
 # Buttons
 submit_button = ctk.CTkButton(
     frame,
-    text="Submit",
+    text="Submit",fg_color="darkgreen",hover_color="green",
     command=lambda: [submit_action(), app.quit()],
     font=("Raanan", 20)
 )
 
 submit_button.grid(row=3, column=1, padx=30, pady=20)
-exit_button = ctk.CTkButton(frame, text="Exit", command=app.quit, font=("ranan", 20))
+exit_button = ctk.CTkButton(frame, text="Exit", command=app.quit, font=("ranan", 20),hover_color="red", fg_color="darkred")
 exit_button.grid(row=3, column=0, padx=30, pady=20)
-tos_button = ctk.CTkButton(frame, text="Terms of Service", font=("ranan", 20))
+tos_button = ctk.CTkButton(frame, text="Terms of Service", font=("ranan", 20),hover_color="grey", fg_color="black",border_color="white",border_width=1)
 tos_button.grid(row=4, column=0, padx=100, pady=10, sticky="news", columnspan=2)
-privacy_button = ctk.CTkButton(frame, text="Privacy Policy", font=("ranan", 20))
+privacy_button = ctk.CTkButton(frame, text="Privacy Policy", font=("ranan", 20),hover_color="grey", fg_color="black",border_color="white",border_width=1)
 privacy_button.grid(row=5, column=0, padx=100,pady=10, sticky="news", columnspan=2)
 
 
